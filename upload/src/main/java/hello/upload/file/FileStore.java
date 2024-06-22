640c0e79d3c4d19597bda4539886c85579c5d486
chaebin <chaebin421@gmail.com>
2024-06-23 01:42:06 +0900

    section11. 파일 업로드

---
+++ upload/src/main/java/hello/upload/file/FileStore.java
@@ -0,0 +1,72 @@
+package hello.upload.file;
+
+import hello.upload.domain.UploadFile;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Component
+public class FileStore {
+
+    @Value("${file.dir}")
+    private String fileDir;
+
+    public String getFullPath(String filename) {
+        return fileDir + filename;
+    }
+
+    public List<UploadFile> storeFiles(List<MultipartFile> multipartFiles) throws IOException {
+
+        List<UploadFile> storeFileResult = new ArrayList<>();
+
+        for (MultipartFile multipartFile : multipartFiles) {
+            if (!multipartFile.isEmpty()) {
+                storeFileResult.add(storeFile(multipartFile));
+            }
+        }
+
+        return storeFileResult;
+    }
+
+    /**
+     * MultipartFile을 받아서 파일을 저장한 후 UploadFile로 리턴
+     */
+    public UploadFile storeFile(MultipartFile multipartFile) throws IOException {
+
+        if (multipartFile.isEmpty()) {
+            return null;
+        }
+
+        // 진짜 파일명
+        String originalFilename = multipartFile.getOriginalFilename();
+        // 서버에 저장될 파일명
+        String storeFileName = createStoreFileName(originalFilename);
+
+        multipartFile.transferTo(new File(getFullPath(storeFileName)));
+
+        return new UploadFile(originalFilename, storeFileName);
+    }
+
+    /**
+     * 서버에 저장될 파일명을 생성
+     */
+    private String createStoreFileName(String originalFilename) {
+        String ext = extractExt(originalFilename);
+        String uuid = UUID.randomUUID().toString();
+        return uuid + "." + ext;
+    }
+
+    /**
+     * 확장자를 리턴
+     */
+    private String extractExt(String originalFilename) {
+        int pos = originalFilename.lastIndexOf(".");
+        return originalFilename.substring(pos + 1);
+    }
+}
