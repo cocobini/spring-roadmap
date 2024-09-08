ff92668351bc6a6a863c54a432e2fcb2678c68f0
chaebin <chaebin421@gmail.com>
2024-09-08 10:37:18 +0900

    section1. 데이터 접근 기술 - 시작

---
+++ itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -0,0 +1,26 @@
+package hello.itemservice;
+
+import hello.itemservice.config.*;
+import hello.itemservice.repository.ItemRepository;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
+
+
+@Import(MemoryConfig.class)
+@SpringBootApplication(scanBasePackages = "hello.itemservice.web")
+public class ItemServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemServiceApplication.class, args);
+	}
+
+	@Bean
+	@Profile("local") // 특정 profile이 활성화되어있는 경우에만 작동
+	public TestDataInit testDataInit(ItemRepository itemRepository) {
+		return new TestDataInit(itemRepository);
+	}
+
+}
