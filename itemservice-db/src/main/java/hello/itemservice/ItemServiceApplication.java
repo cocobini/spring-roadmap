6f5e651bd2762a9e11cec2c57d36d40c3703fbc8
chaebin <chaebin421@gmail.com>
2024-09-22 13:49:44 +0900

    section3. 데이터 접근 기술 - 테스트

--- itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -8,6 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 @Slf4j
 //@Import(MemoryConfig.class)
@@ -26,5 +29,18 @@ public class ItemServiceApplication {
 	public TestDataInit testDataInit(ItemRepository itemRepository) {
 		return new TestDataInit(itemRepository);
 	}
+
+	// 별도의 설정이 없으면 임베디드 모드로 동작
+//	@Bean
+//	@Profile("test")
+//	public DataSource dataSource() {
+//		log.info("메모리 데이터베이스 초기화");
+//		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//		dataSource.setDriverClassName("org.h2.Driver");
+//		dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1"); // 커넥션이 모두 끊어져도 DB가 종료되지 않게 옵션을 줌
+//		dataSource.setUsername("sa");
+//		dataSource.setPassword("");
+//		return dataSource;
+//	}
 
 }
--- itemservice-db/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
+++ itemservice-db/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
@@ -5,26 +5,51 @@ import hello.itemservice.repository.ItemSearchCond;
 import hello.itemservice.repository.ItemUpdateDto;
 import hello.itemservice.repository.memory.MemoryItemRepository;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+//@Commit // 롤백이 아닌 강제커밋을 하려면 해당 어노테이션 사요
+@Transactional // 트랜잭션 처리를 자동으로 해줌(테스트에서 동작하면 롤백이 기본 설정)
+@SpringBootTest // @SpringBootApplication 어노테이션을 찾아서 실행
 class ItemRepositoryTest {
 
     @Autowired
     ItemRepository itemRepository;
 
+    // 테스트는 다른 테스트와 격리해야 함
+    // 테스트는 반복해서 실행할 수 있어야 함
+
+    // 트랜잭션 관련 코드
+    @Autowired
+    PlatformTransactionManager transactionManager;
+    TransactionStatus status;
+
+//    @BeforeEach
+//    void beforeEach() {
+//        // 트랜잭션 시작
+//        status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+//    }
+
     @AfterEach
     void afterEach() {
-        //MemoryItemRepository 의 경우 제한적으로 사용
+        // MemoryItemRepository 의 경우 제한적으로 사용
         if (itemRepository instanceof MemoryItemRepository) {
             ((MemoryItemRepository) itemRepository).clearStore();
         }
+
+        // 트랜잭션 롤백
+        // transactionManager.rollback(status);
     }
 
     @Test
@@ -36,7 +61,7 @@ class ItemRepositoryTest {
         Item savedItem = itemRepository.save(item);
 
         //then
-        Item findItem = itemRepository.findById(item.getId()).get();
+        Item findItem = itemRepository.findById(item.getId()).get(); // 커밋하지 않아도  내 트랜잭션 내에서는 조회 가능
         assertThat(findItem).isEqualTo(savedItem);
     }
 
@@ -69,19 +94,19 @@ class ItemRepositoryTest {
         itemRepository.save(item2);
         itemRepository.save(item3);
 
-        //둘 다 없음 검증
+        // 둘 다 없음 검증
         test(null, null, item1, item2, item3);
         test("", null, item1, item2, item3);
 
-        //itemName 검증
+        // itemName 검증
         test("itemA", null, item1, item2);
         test("temA", null, item1, item2);
         test("itemB", null, item3);
 
-        //maxPrice 검증
+        // maxPrice 검증
         test(null, 10000, item1);
 
-        //둘 다 있음 검증
+        // 둘 다 있음 검증
         test("itemA", 10000, item1);
     }
 
