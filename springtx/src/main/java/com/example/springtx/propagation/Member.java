ac2cc94ac20ca72905461144489114a5e4c3e663
chaebin <chaebin421@gmail.com>
2024-11-16 23:49:34 +0900

    section11. 스프링 트랜잭션 전파2 - 활용

---
+++ springtx/src/main/java/com/example/springtx/propagation/Member.java
@@ -0,0 +1,22 @@
+package com.example.springtx.propagation;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String username;
+
+    public Member() {
+    }
+
+    public Member(String username) {
+        this.username = username;
+    }
+}
